abe6bb96392f5cc6c81a8f752b9acf18cadae9c4
Davis-don <davismugoikou@gmail.com>
2024-12-08 19:44:28 +0300

    added arrays and objects

---
+++ day2/ObjectsArrays/codeme.ts
@@ -0,0 +1,34 @@
+//arrays
+const names = [20,"Annisia","benson","kariuki"]
+//to push new element in array
+names.push("Daniel")
+//try push value of different type u incure an error die to strict type
+names.push(true)
+
+console.log(names)
+
+//for mixed mix up the values inputed
+
+//objects
+
+let student = {
+    name:"Davis",
+    age:20,
+    presence:false
+}
+
+//try updating the object to contain different datatypes foe a field 
+student.name = 30   //try updating the object to contain different datatypes foe a field error
+
+student.name = "anisia"  //what about correct update eg
+console.log(student)  //this works
+
+student = {
+    name:"Davis"   //error shows that it doesnt have original structure of object
+}
+
+student={
+    name:"An",
+    age:30,           //correct structure
+    presence:true
+}
