c0cc4bb5819284d0b65e04c1064aced48fb53ee9
Davis-don <davismugoikou@gmail.com>
2024-12-08 20:41:43 +0300

    any keyword

---
+++ day2/dynamicAny/codeme.js
@@ -0,0 +1,4 @@
+//declate variable of any type
+var age = 25;
+age = true;
+console.log(age);
---
+++ day2/dynamicAny/codeme.ts
@@ -0,0 +1,7 @@
+//declate variable of any type
+let age:any=25;
+age=true;
+console.log(age)
+
+let mixed:any[] //any datatype 
+
